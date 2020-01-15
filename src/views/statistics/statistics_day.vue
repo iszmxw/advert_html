6eb2f956debdccf8f210dd16ed2d084953e4a530
iszmxw <mail@54zm.com>
2020-01-15 23:56:44 +0800

    先下拉=>后上传

---
+++ src/views/statistics/statistics_day.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dashboard-editor-container">
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData" />
+    </el-row>
+  </div>
+</template>
+
+<script>
+import LineChart from './components/LineChart'
+
+const lineChartData = {
+  day: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  }
+}
+
+export default {
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      lineChartData: lineChartData.day
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+}
+</style>
