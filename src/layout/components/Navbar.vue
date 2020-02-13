0345ad16e952fa784e29d103a67b7d4ee3e91969
queya <wu775825@163.com>
2020-02-13 17:08:09 +0800

    将平台登录后的默认头像改为平台的logo

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="headImg" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
@@ -34,11 +34,17 @@
 import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
+import images1 from '@/assets/img/logo2.jpg'
 
 export default {
   components: {
     Breadcrumb,
     Hamburger
+  },
+  data() {
+    return {
+      headImg: images1
+    }
   },
   computed: {
     ...mapGetters([
